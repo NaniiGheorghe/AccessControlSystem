91be1577c5761d7bfbb0848881a86374c2c9135a
gnanii <gnanii@inthergroup.com>
2019-06-13 00:47:04 +0300

    Fixed Key

--- project/AccessControlSystem-Server/src/main/java/com/acs/model/Key.java
+++ project/AccessControlSystem-Server/src/main/java/com/acs/model/Key.java
@@ -9,7 +9,7 @@ public class Key {
 
     @Id
     @Column(name = "id")
-    @GeneratedValue(strategy=GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     private String name;
